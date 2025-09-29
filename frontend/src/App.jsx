ba28264bc6b6d970c132c4363c355ea06acbc781
mdjameel04 <jameeltony67@gmail.com>
2025-09-29 10:16:14 +0530

    added featuer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-import Button from './Components/button'
+
 
 const App = () => {
   return (
     <div>
-<Button/>
+<Navbar/>
 </div>
 
   )
---
+++ frontend/src/assets/assests.js
@@ -0,0 +1,21 @@
+import logo from './Logo.png'
+import menu from './menu-burger.png'
+import Logo_1 from './main-logo.png'
+
+
+export const assests= {
+    logo,
+    menu,
+    Logo_1
+}
+
+export const navItems = [
+ {name : "Home" , path:'/'},
+ {name : "Shop" , path:'/Shop'},
+{name : "About Us" , path:'/About'},
+{name : "Blog" , path:'/Blog'},    
+ {name : "Contact" , path:'/Contact'},
+
+
+
+];
