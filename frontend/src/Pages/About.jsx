52a4cd1dc1db1a415df4c04ce89a58929b4a180c
mdjameel04 <jameeltony67@gmail.com>
2025-10-07 22:10:00 +0530

    inital commit

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { Swiper,SwiperSlide } from 'swiper/react'
 import { FaHome} from 'react-icons/fa'
 import {MdRestaurantMenu} from "react-icons/md"
 import { Link } from 'react-router-dom'
+import { EffectCoverflow, Autoplay } from 'swiper/modules'
 
 const About = () => {
+   const images = [
+      "https://images.unsplash.com/photo-1555396273-367ea4eb4db5?q=80&w=600",
+    "https://images.unsplash.com/photo-1528697203043-733dafdaa316?q=80&w=600",
+    "https://images.unsplash.com/photo-1528697203043-733dafdaa316?q=80&w=600",
+    "https://images.unsplash.com/photo-1578683010236-d716f9a3f461?q=80&w=600",
+    "https://images.unsplash.com/photo-1578683010236-d716f9a3f461?q=80&w=600",
+  ];
   return (
     <section className="bg-[bisque] p-4"   > 
       {/* heading */}
@@ -24,12 +32,54 @@ const About = () => {
 
 <div className='flex items-center justify-center'>
   <Link to={'/shop'}>
-<button className='flex items-center bg-gradient-to-l bg-pink-600 to-blue-400 rounded-lg px-4 py-2 mt-4'>
-  <MdRestaurantMenu/>  explore It
+<button className='flex items-center gap-2 bg-gradient-to-l bg-pink-600 to-blue-400 rounded-2xl px-4 py-2 mt-4 font-medium'>
+  <MdRestaurantMenu/>  Explore It
 </button>
   </Link>
 </div>
      </div>
+
+
+{/* images */}
+
+<div className="w-full flex justify-center items-center bg-white py-10">
+      <Swiper
+        modules={[EffectCoverflow, Autoplay]}
+        effect="coverflow"
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true} // 🔁 Makes it circular
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 200,
+          modifier: 2.5,
+          scale: 0.9,
+          slideShadows: false,
+        }}
+        className="w-[90%] max-w-5xl"
+      >
+        {images.map((src, i) => (
+          <SwiperSlide
+            key={i}
+            className="!w-[280px] !h-[200px] flex justify-center items-center"
+          >
+            <img
+              src={src}
+              alt={`slide-${i}`}
+              className="w-full h-full object-cover rounded-2xl shadow-xl transition-transform duration-300"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+
+
 
     </section>
   )
