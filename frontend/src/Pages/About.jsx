9b6f270e96b343ab734bafd09806c68be631676e
mdjameel04 <jameeltony67@gmail.com>
2025-10-08 20:58:36 +0530

    added featuer

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -7,21 +7,6 @@ import { EffectCoverflow, Autoplay } from 'swiper/modules'
 
 const About = () => {
    const images = [
-      "https://images.unsplash.com/photo-1528697203043-733dafdaa316?q=80&w=600",
-  "https://images.unsplash.com/photo-1564758866811-28b4b59d7e6b?q=80&w=600",
-  "https://images.unsplash.com/photo-1578683010236-d716f9a3f461?q=80&w=600",
-  "https://images.unsplash.com/photo-1582582494700-1c1c98ff8a58?q=80&w=600",
-  "https://images.unsplash.com/photo-1604014217032-3b3e6e8c0b1d?q=80&w=600",
-  "https://images.unsplash.com/photo-1598300054796-02b448d3542a?q=80&w=600",
-  "https://images.unsplash.com/photo-1616627987786-1e512e9cf138?q=80&w=600",
-  "https://images.unsplash.com/photo-1582719478149-123eeb3a3bfb?q=80&w=600",
-  "https://images.unsplash.com/photo-1605902711622-cfb43c443e92?q=80&w=600",
-  "https://images.unsplash.com/photo-1616627346103-fc2b4f26a0b5?q=80&w=600",
-  "https://images.unsplash.com/photo-1602524207463-1b4decd9e3e1?q=80&w=600",
-  "https://images.unsplash.com/photo-1616627410168-9a5f37e0576b?q=80&w=600",
-  "https://images.unsplash.com/photo-1602524207446-8fdbf27b43d2?q=80&w=600",
-  "https://images.unsplash.com/photo-1616627410234-cbf048bfa6e8?q=80&w=600",
-  "https://images.unsplash.com/photo-1602524207401-b39e21b3e0fa?q=80&w=600"
    ]
   return (
     <section className="bg-[bisque] p-4"   > 
